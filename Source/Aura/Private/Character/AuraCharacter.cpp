71308fb4934100311f9469d979a225392fbfeaba
WonJeong <liclem@naver.com>
2024-07-19 18:15:11 +0900

    Add GameMode and Update Anim BP

--- Source/Aura/Private/Character/AuraCharacter.cpp
+++ Source/Aura/Private/Character/AuraCharacter.cpp
@@ -3,3 +3,16 @@
 
 #include "Character/AuraCharacter.h"
 
+#include "GameFramework/CharacterMovementComponent.h"
+
+AAuraCharacter::AAuraCharacter()
+{
+	GetCharacterMovement()->bOrientRotationToMovement = true;
+	GetCharacterMovement()->RotationRate = FRotator(0.f, 400.f, 0.f);
+	GetCharacterMovement()->bConstrainToPlane = true;	// 캐릭터의 이동을 평면으로 제한
+	GetCharacterMovement()->bSnapToPlaneAtStart = true;	// 시작할 때 캐릭터의 위치가 평면을 벗어난 상태라면 가까운 평면에 붙여서 시작
+
+	bUseControllerRotationPitch = false;
+	bUseControllerRotationRoll = false;
+	bUseControllerRotationYaw = false;
+}
