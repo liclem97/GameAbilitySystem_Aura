09830f8b7e4ed5bb92ff10db1a4f683672fdc700
WonJeong <liclem@naver.com>
2024-08-09 18:22:38 +0900

    Create AuraGameplayTags class

---
+++ Source/Aura/Private/AuraGameplayTags.cpp
@@ -0,0 +1,12 @@
+
+
+
+#include "AuraGameplayTags.h"
+
+AuraGameplayTags::AuraGameplayTags()
+{
+}
+
+AuraGameplayTags::~AuraGameplayTags()
+{
+}
---
+++ Source/Aura/Public/AuraGameplayTags.h
@@ -0,0 +1,15 @@
+
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+/**
+ * 
+ */
+class AURA_API AuraGameplayTags
+{
+public:
+	AuraGameplayTags();
+	~AuraGameplayTags();
+};
