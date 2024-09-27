132af56fe911c96152347236146f5c7ce1784e46
WonJeong <liclem@naver.com>
2024-09-27 16:22:41 +0900

    Added XP to the Player State

--- Source/Aura/Public/AbilitySystem/Data/LevelUpInfo.h
+++ Source/Aura/Public/AbilitySystem/Data/LevelUpInfo.h
@@ -22,7 +22,16 @@ struct FAuraLevelUpInfo
 };
 
 /**
- * 
+ * ULevelUpInfo 클래스
+ *
+ * 이 클래스는 캐릭터 레벨업 정보를 관리하는 Data Asset.
+ * 각 레벨에 필요한 경험치 요구사항과 레벨업 시 획득하는 속성 포인트 및 스펠 포인트에 대한 정보를 제공.
+ *
+ * 주요 역할:
+ * - 레벨업에 필요한 정보를 저장하고 관리.
+ * - 주어진 경험치를 기반으로 캐릭터의 현재 레벨을 찾는 기능을 제공.
+ *
+ * @see UDataAsset
  */
 UCLASS()
 class AURA_API ULevelUpInfo : public UDataAsset
--- Source/Aura/Public/Player/AuraPlayerState.h
+++ Source/Aura/Public/Player/AuraPlayerState.h
@@ -7,11 +7,24 @@
 #include "GameFramework/PlayerState.h"
 #include "AuraPlayerState.generated.h"
 
+DECLARE_MULTICAST_DELEGATE_OneParam(FOnPlayerStatChanged, int32 /*StatValue*/)
+
 class UAbilitySystemComponent;
 class UAttributeSet;
 
 /**
- * 
+ * AAuraPlayerState 클래스
+ *
+ * 이 클래스는 플레이어 상태를 관리하는 APlayerState의 확장.
+ * 플레이어의 능력 시스템과 속성, 경험치(XP), 레벨 등의 정보를 관리하며,
+ * 경험치 및 레벨 변화에 따른 델리게이트를 제공합니다.
+ *
+ * 주요 역할:
+ * - 플레이어의 레벨 및 경험치 관리.
+ * - 능력 시스템 컴포넌트와 속성(AttributeSet) 접근 제공.
+ * - 경험치와 레벨 변경 시 이를 다른 시스템에 알리기 위한 델리게이트 호출.
+ *
+ * @see APlayerState, IAbilitySystemInterface
  */
 UCLASS()
 class AURA_API AAuraPlayerState : public APlayerState, public IAbilitySystemInterface
@@ -24,7 +37,17 @@ public:
 	virtual UAbilitySystemComponent* GetAbilitySystemComponent() const override;
 	UAttributeSet* GetAttributeSet() const { return AttributeSet; }
 
+	FOnPlayerStatChanged OnXPChangedDelegate;
+	FOnPlayerStatChanged OnLevelChangedDelegate;
+
 	FORCEINLINE int32 GetPlayerLevel() const { return Level; }
+	FORCEINLINE int32 GetXP() const { return XP; }
+
+	void AddToXP(int32 InXP);
+	void AddToLevel(int32 InLevel);
+
+	void SetXP(int32 InXP);
+	void SetLevel(int32 InLevel);
 
 protected:
 	UPROPERTY(VisibleAnywhere)
@@ -37,6 +60,12 @@ private:
 	UPROPERTY(VisibleAnywhere, ReplicatedUsing = OnRep_Level)
 	int32 Level = 1;
 
+	UPROPERTY(VisibleAnywhere, ReplicatedUsing = OnRep_XP)
+	int32 XP = 1;
+
 	UFUNCTION()
 	void OnRep_Level(int32 OldLevel);
+
+	UFUNCTION()
+	void OnRep_XP(int32 OldXP);
 };
