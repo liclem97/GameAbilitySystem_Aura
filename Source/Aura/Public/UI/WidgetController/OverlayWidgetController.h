6f36cb4fd91d33150ee229faffbd80b3b46fe302
WonJeong <liclem@naver.com>
2024-10-03 16:11:48 +0900

    Showing Level in the HUD

--- Source/Aura/Public/UI/WidgetController/OverlayWidgetController.h
+++ Source/Aura/Public/UI/WidgetController/OverlayWidgetController.h
@@ -30,6 +30,7 @@ struct FOnAttributeChangeData;
 class UAbilityInfo;
 class UAuraAbilitySystemComponent;
 
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnPlayerStatChangedSignature, int32, NewValue);
 DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnAttributeChangedSignature, float, NewValue);
 DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FMessageWidgetRowSignature, FUIWidgetRow, Row);
 DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FAbilityInfoSignature, const FAuraAbilityInfo&, Info);
@@ -38,23 +39,8 @@ DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FAbilityInfoSignature, const FAuraAb
  * UOverlayWidgetController 클래스
  *
  * 이 클래스는 언리얼 엔진의 위젯 컨트롤러를 상속받아 오버레이 위젯과 관련된 UI 요소들을 관리하는 역할을 담당.
- * 주로 플레이어의 상태(체력, 마나 등) 및 메시지 위젯과 같은 UI 컴포넌트를 갱신하고, 이를 블루프린트에서 쉽게 사용할 수 있도록 함.
+ * 주로 플레이어의 상태(체력, 마나, 경험치, 레벨 등) 및 메시지 위젯과 같은 UI 컴포넌트를 갱신하고, 이를 블루프린트에서 쉽게 사용할 수 있도록 함.
  *
- * 주요 기능:
- * - 초기값을 브로드캐스트하여 UI의 기본 상태를 설정.
- * - 속성(Attributes) 변화에 따라 UI 요소들을 업데이트하고, 속성 변화 이벤트를 델리게이트로 전달.
- * - 메시지 위젯에 대한 데이터를 처리하고 메시지를 UI에 표시.
- *
- * 제공 기능:
- * - OnHealthChanged, OnMaxHealthChanged, OnManaChanged 등 플레이어 속성 변화에 따른 델리게이트.
- * - MessageWidgetRowDelegate를 통해 UI에 메시지와 관련된 데이터를 표시.
- *
- * 제공 메서드:
- * - BroadcastInitialValues(): 위젯의 초기값을 브로드캐스트하는 함수.
- * - BindCallbacksToDependencies(): UI 요소에 필요한 콜백을 바인딩하는 함수.
- * - GetDataTableRowByTag(): 데이터 테이블에서 태그를 기준으로 데이터를 가져오는 템플릿 함수.
- *
- * @see UAuraWidgetController
  */
 UCLASS(BlueprintType, Blueprintable)
 class AURA_API UOverlayWidgetController : public UAuraWidgetController
@@ -89,6 +75,9 @@ public:
 
 	UPROPERTY(BlueprintAssignable, Category = "GAS|XP")
 	FOnAttributeChangedSignature OnXPPercentChangedDelegate;
+
+	UPROPERTY(BlueprintAssignable, Category = "GAS|Level")
+	FOnPlayerStatChangedSignature OnPlayerLevelChangedDelegate;
 
 protected:
 	// 메시지 위젯 관련 데이터를 저장하는 데이터 테이블
