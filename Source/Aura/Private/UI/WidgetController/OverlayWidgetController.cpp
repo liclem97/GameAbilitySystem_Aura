ad0e859e7916e683de40a547adcfd5d51cc4915d
WonJeong <liclem@naver.com>
2024-10-15 19:54:00 +0900

    Updating the Overlay When Equipping Abilities

--- Source/Aura/Private/UI/WidgetController/OverlayWidgetController.cpp
+++ Source/Aura/Private/UI/WidgetController/OverlayWidgetController.cpp
@@ -5,7 +5,9 @@
 
 #include "AbilitySystem/AuraAbilitySystemComponent.h"
 #include "AbilitySystem/AuraAttributeSet.h"
+#include "AbilitySystem/Data/AbilityInfo.h"
 #include "AbilitySystem/Data/LevelUpInfo.h"
+#include "AuraGameplayTags.h"
 #include "Player/AuraPlayerState.h"
 
 void UOverlayWidgetController::BroadcastInitialValues()
@@ -56,6 +58,7 @@ void UOverlayWidgetController::BindCallbacksToDependencies()
 		);
 	if (GetAuraASC())
 	{	
+		GetAuraASC()->AbilityEquipped.AddUObject(this, &UOverlayWidgetController::OnAbilityEquipped);
 		if (GetAuraASC()->bStartupAbilitiesGiven)
 		{
 			BroadcastAbilityInfo();
@@ -111,4 +114,24 @@ void UOverlayWidgetController::OnXPChanged(int32 NewXP)
 		// 계산된 퍼센트를 델리게이트로 브로드캐스트.
 		OnXPPercentChangedDelegate.Broadcast(XPBarPercent);
 	}
+}
+
+void UOverlayWidgetController::OnAbilityEquipped(const FGameplayTag& AbilityTag, const FGameplayTag& Status, const FGameplayTag& Slot, const FGameplayTag& PreviousSlot) const
+{
+	const FAuraGameplayTags& GameplayTags = FAuraGameplayTags::Get();
+
+	// 이전 슬롯을 클리어.
+	FAuraAbilityInfo LastSlotInfo;
+	LastSlotInfo.StatusTag = GameplayTags.Abilities_Status_Unlocked;
+	LastSlotInfo.InputTag = PreviousSlot;
+	LastSlotInfo.AbilityTag = GameplayTags.Abilities_None;
+
+	// 이전 슬롯이 유효한 경우 빈 정보 브로드캐스트. (이미 장착된 스펠을 다시 장착할 때만)
+	AbilityInfoDelegate.Broadcast(LastSlotInfo);
+
+	// 새로운 슬롯에 능력 정보 설정.
+	FAuraAbilityInfo Info = AbilityInfo->FindAbilityInfoForTag(AbilityTag);
+	Info.StatusTag = Status;
+	Info.InputTag = Slot;
+	AbilityInfoDelegate.Broadcast(Info);
 }
