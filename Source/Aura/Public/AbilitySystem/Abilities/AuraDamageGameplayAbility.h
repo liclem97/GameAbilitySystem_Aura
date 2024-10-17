9c637162b965fedc378b9c31ed104996e9b14479
WonJeong <liclem@naver.com>
2024-10-17 17:30:27 +0900

    Add Debuff Parameters

--- Source/Aura/Public/AbilitySystem/Abilities/AuraDamageGameplayAbility.h
+++ Source/Aura/Public/AbilitySystem/Abilities/AuraDamageGameplayAbility.h
@@ -8,7 +8,10 @@
 #include "AuraDamageGameplayAbility.generated.h"
 
 /**
- * 
+ * UAuraDamageGameplayAbility 클래스
+ *
+ * 어빌리티의 데미지 요소를 담당하는 클래스.
+ * 데미지 타입, 데미지, 디버프 확률, 디버프 데미지, 디버프 주기, 디버프 시간 등을 결정함. 
  */
 UCLASS()
 class AURA_API UAuraDamageGameplayAbility : public UAuraGameplayAbility
@@ -28,6 +31,18 @@ protected:
 
 	UPROPERTY(EditDefaultsOnly, Category = "Damage")
 	FScalableFloat Damage;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Damage")
+	float DebuffChance = 20.f;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Damage")
+	float DebuffDamage = 5.f;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Damage")
+	float DebuffFrequency = 1.f;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Damage")
+	float DebuffDuration = 5.f;
 
 	UFUNCTION(BlueprintPure)
 	FTaggedMontage GetRandomTaggedMontagFromArray(const TArray<FTaggedMontage>& TaggedMontages) const;
