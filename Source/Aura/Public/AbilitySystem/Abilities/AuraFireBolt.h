3ece025797e4d47baaabfc12aeb0fa35cbba0a19
WonJeong <liclem@naver.com>
2024-10-22 17:23:52 +0900

    Make Spread of FireBolt

--- Source/Aura/Public/AbilitySystem/Abilities/AuraFireBolt.h
+++ Source/Aura/Public/AbilitySystem/Abilities/AuraFireBolt.h
@@ -18,4 +18,13 @@ public:
 	virtual FString GetDescription(int32 Level) override;
 	virtual FString GetNextLevelDescription(int32 Level) override;
 
+	UFUNCTION(BlueprintCallable)
+	void SpawnProjectiles(const FVector& ProjectileTargetLocation, const FGameplayTag& SocketTag, bool bOverridePitch, float PitchOverride, AActor* HomingTarget);
+
+protected:
+	UPROPERTY(EditDefaultsOnly, Category = "FireBolt")
+	float ProjectileSpread = 90.f;
+
+	UPROPERTY(EditDefaultsOnly, Category = "FireBolt")
+	int32 MaxNumProjectiles = 5;
 };
