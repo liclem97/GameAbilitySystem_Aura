bfb0e43e68991b5ac9e856a373b9081c028ac199
WonJeong <liclem@naver.com>
2024-10-24 16:29:33 +0900

    Create AuraBeamSpell

---
+++ Source/Aura/Private/AbilitySystem/Abilities/AuraBeamSpell.cpp
@@ -0,0 +1,25 @@
+
+
+
+#include "AbilitySystem/Abilities/AuraBeamSpell.h"
+
+void UAuraBeamSpell::StoreMouseDataInfo(const FHitResult& HitResult)
+{
+	if (HitResult.bBlockingHit)
+	{
+		MouseHitLocation = HitResult.ImpactPoint;
+		MouseHitActor = HitResult.GetActor();
+	}
+	else
+	{
+		CancelAbility(CurrentSpecHandle, CurrentActorInfo, CurrentActivationInfo, true);
+	}
+}
+
+void UAuraBeamSpell::StoreOwnerPlayerContrller()
+{	
+	if (CurrentActorInfo)
+	{
+		OwnerPlayerController = CurrentActorInfo->PlayerController.Get();
+	}
+}
