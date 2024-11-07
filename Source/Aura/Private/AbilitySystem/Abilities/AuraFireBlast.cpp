a8aa7e7ed1edbdc9642e80be7b2d8cd9a44c2aa7
WonJeong <liclem@naver.com>
2024-11-07 17:30:44 +0900

    Create FireBlast Ability

---
+++ Source/Aura/Private/AbilitySystem/Abilities/AuraFireBlast.cpp
@@ -0,0 +1,70 @@
+
+
+
+#include "AbilitySystem/Abilities/AuraFireBlast.h"
+
+FString UAuraFireBlast::GetDescription(int32 Level)
+{
+	const int32 ScaledDamge = Damage.GetValueAtLevel(Level);
+	const float ManaCost = FMath::Abs(GetManaCost(Level));
+	const float Cooldown = GetCooldown(Level);
+	return FString::Printf(TEXT(
+		// Title
+		"<Title>FIRE BLAST</>\n\n"
+
+		// Level
+		"<Small>Level: </><Level>%d</>\n"
+		// ManaCost
+		"<Small>ManaCost: </><ManaCost>%.1f</>\n"
+		// Cooldown
+		"<Small>Cooldown: </><Cooldown>%.1f</>\n\n"
+
+		// Number Of Fire Balls
+		"<Default>Lauches %d </>"
+		"<Default>fire balls in all directions, each coming back and</>"
+		"<Default>exploding upon return, causing</>"
+
+		// Damage
+		"<Damage> %d</><Default> radial fire damage with"
+		" a chance to burn</>"),
+
+		// Values
+		Level,
+		ManaCost,
+		Cooldown,
+		NumFireBalls,
+		ScaledDamge);
+}
+
+FString UAuraFireBlast::GetNextLevelDescription(int32 Level)
+{
+	const int32 ScaledDamge = Damage.GetValueAtLevel(Level);
+	const float ManaCost = FMath::Abs(GetManaCost(Level));
+	const float Cooldown = GetCooldown(Level);
+	return FString::Printf(TEXT(
+		// Title
+		"<Title>NEXT LEVEL:</>\n\n"
+
+		// Level
+		"<Small>Level: </><Level>%d</>\n"
+		// ManaCost
+		"<Small>ManaCost: </><ManaCost>%.1f</>\n"
+		// Cooldown
+		"<Small>Cooldown: </><Cooldown>%.1f</>\n\n"
+
+		// Number Of Fire Balls
+		"<Default>Lauches %d </>"
+		"<Default>fire balls in all directions, each coming back and</>"
+		"<Default>exploding upon return, causing</>"
+
+		// Damage
+		"<Damage> %d</><Default> radial fire damage with"
+		" a chance to burn</>"),
+
+		// Values
+		Level,
+		ManaCost,
+		Cooldown,
+		NumFireBalls,
+		ScaledDamge);
+}
