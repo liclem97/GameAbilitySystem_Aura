43dc1f45337b374e21f66251e8ec96b9f07a1322
WonJeong <liclem@naver.com>
2024-11-07 18:50:54 +0900

    Make Fireballs Causing Damage

--- Source/Aura/Private/Actor/AuraFireBall.cpp
+++ Source/Aura/Private/Actor/AuraFireBall.cpp
@@ -3,6 +3,9 @@
 
 #include "Actor/AuraFireBall.h"
 
+#include "AbilitySystem/AuraAbilitySystemLibrary.h"
+#include "AbilitySystemBlueprintLibrary.h"
+
 void AAuraFireBall::BeginPlay()
 {
 	Super::BeginPlay();
@@ -12,5 +15,18 @@ void AAuraFireBall::BeginPlay()
 void AAuraFireBall::OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
 	UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
 {
+	if (!IsValidOverlap(OtherActor)) return;
 
+	if (HasAuthority())
+	{
+		if (UAbilitySystemComponent* TargetASC = UAbilitySystemBlueprintLibrary::GetAbilitySystemComponent(OtherActor))
+		{
+			const FVector DeathImpulse = GetActorForwardVector() * DamageEffectParams.DeathImpulseMagnitude;
+			DamageEffectParams.DeathImpulse = DeathImpulse;
+			
+			// 이곳에서 타겟ASC를 설정해줌.
+			DamageEffectParams.TargetAbilitySystemComponent = TargetASC;
+			UAuraAbilitySystemLibrary::ApplyDamageEffect(DamageEffectParams);
+		}
+	}
 }
