5c380d9d82ec961ab97a8f2230b928f5a4994602
WonJeong <liclem@naver.com>
2024-11-19 18:27:37 +0900

    Saving the Map Name

--- Source/Aura/Private/UI/ViewModel/MVVM_LoadScreen.cpp
+++ Source/Aura/Private/UI/ViewModel/MVVM_LoadScreen.cpp
@@ -88,5 +88,6 @@ void UMVVM_LoadScreen::LoadData()
 		LoadSlot.Value->SlotStatus = SaveSlotStatus;
 		LoadSlot.Value->SetPlayerName(PlayerName);
 		LoadSlot.Value->InitializeSlot();
+		LoadSlot.Value->SetMapName(SaveObject->MapName);
 	}
 }
