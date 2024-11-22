ff69accd027b520a2b3b3b53a166bb9f01551411
WonJeong <liclem@naver.com>
2024-11-22 16:23:16 +0900

    Choosing the Player Start

--- Source/Aura/Private/Game/AuraGameModeBase.cpp
+++ Source/Aura/Private/Game/AuraGameModeBase.cpp
@@ -4,6 +4,7 @@
 #include "Game/AuraGameModeBase.h"
 
 #include "Game/LoadScreenSaveGame.h"
+#include "GameFramework/PlayerStart.h"
 #include "Kismet/GameplayStatics.h"
 #include "UI/ViewModel/MVVM_LoadSlot.h"
 
@@ -54,6 +55,36 @@ void AAuraGameModeBase::TravelToMap(UMVVM_LoadSlot* Slot)
 	const int32 SlotIndex = Slot->SlotIndex;
 
 	UGameplayStatics::OpenLevelBySoftObjectPtr(Slot, Maps.FindChecked(Slot->GetMapName()));
+}
+
+AActor* AAuraGameModeBase::ChoosePlayerStart_Implementation(AController* Player)
+{
+	TArray<AActor*> Actors;
+
+	// 월드에 있는 PlayerStart 객체를 Actors 배열에 저장.
+	UGameplayStatics::GetAllActorsOfClass(GetWorld(), APlayerStart::StaticClass(), Actors);
+
+	// 배열에 저장된 PlayerStart가 있을 경우.
+	if (Actors.Num() > 0)
+	{
+		AActor* SelectedActor = Actors[0];
+
+		// 배열 반복.
+		for (AActor* Actor : Actors)
+		{
+			if (APlayerStart* PlayerStart = Cast<APlayerStart>(Actor))
+			{
+				// 배열의 PlayerStart가 특정 태그를 갖고 있으면 저장 후 반복문 탈출.
+				if (PlayerStart->PlayerStartTag == FName("TheTag"))
+				{
+					SelectedActor = PlayerStart;
+					break;
+				}
+			}
+		}
+		return SelectedActor;
+	}
+	return nullptr;
 }
 
 void AAuraGameModeBase::BeginPlay()
